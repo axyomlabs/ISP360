8024badf794b2b1b7330cda56284899452d1804a
Harshkhatri16 <harshkhatri1975@gmail.com>
2025-08-20 18:14:08 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './components/LoginPage.jsx'
+import DashBoard from './components/DashBoard.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage/>
+    <DashBoard/>
   </StrictMode>,
 )
